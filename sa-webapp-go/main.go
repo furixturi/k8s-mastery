360559999fcd56b698223b7304a657fe2fa1954f
Xiaoli Shen <alabebop@gmail.com>
2021-08-07 22:23:01 +0900

    add docker build ENV to sa-webapp to make sa-logic uri dynamic

--- sa-webapp-go/main.go
+++ sa-webapp-go/main.go
@@ -2,18 +2,23 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 	var (
-		// SA_LOGIC_URL      = os.Getenv("SA_LOGIC_URL")
-		// SA_LOGIC_PORT     = os.Getenv("SA_LOGIC_PORT")
-		// saLogicServiceURI = SA_LOGIC_URL + ":" + SA_LOGIC_PORT + "/analyse/sentiment"
-		// SA_WEBAPP_PORT    = os.Getenv("SA_WEBAPP_PORT")
-		SA_WEBAPP_PORT    = "8080"
-		saLogicServiceURI = "http://localhost:5000/analyse/sentiment"
+		SA_LOGIC_URL      = os.Getenv("SA_LOGIC_URL")
+		SA_LOGIC_PORT     = os.Getenv("SA_LOGIC_PORT")
+		saLogicServiceURI = SA_LOGIC_URL + ":" + SA_LOGIC_PORT + "/analyse/sentiment"
+		SA_WEBAPP_PORT    = os.Getenv("SA_WEBAPP_PORT")
+		// SA_WEBAPP_PORT    = "8080"
+		// saLogicServiceURI = "http://localhost:5000/analyse/sentiment"
 	)
 
-	// fmt.Printf("==== ENV ====\nSA_LOGIC_URL: %s\nSA_LOGIC_PORT: %s\nsaLogicServiceURI: %s\nSA_WEBAPP_PORT: %s\n",
-	// SA_LOGIC_URL, SA_LOGIC_PORT, saLogicServiceURI, SA_WEBAPP_PORT)
+	fmt.Printf("==== ENV ====\nSA_LOGIC_URL: %s\nSA_LOGIC_PORT: %s\nsaLogicServiceURI: %s\nSA_WEBAPP_PORT: %s\n",
+		SA_LOGIC_URL, SA_LOGIC_PORT, saLogicServiceURI, SA_WEBAPP_PORT)
 
 	a := App{}
 
